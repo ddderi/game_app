e729404c26dd12209f0eb3e94675542e003a3165
ddderi <maxime830@live.fr>
2023-02-03 18:40:44 +1000

    export

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Amplify } from "aws-amplify";
-import awsconfig from "./src/aws-exports";
+import awsconfig from "./aws-exports";
 
 Amplify.configure(awsconfig);
 
